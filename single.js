593bfb813ee3da6f7831aeac51e6acc362594be6
Engie-Paquot <angelique.paquot.pro@gmail.com>
2021-08-11 17:05:39 +0200

    Ajout html single, continuation css main 11 aout

---
+++ single.js
@@ -0,0 +1,12 @@
+(async() =>  {
+        const template = document.getElementById("tpl-character")
+        const target = document.getElementById("target")
+        const fetcher = async() => { //on crée une fonction async (pour pouvoir utiliser await)
+            let rawData = await fetch("https://character-database.becode.xyz/characters") // on va chercher les données et on utilise await parce que fetch prend plus de temps pour télécharger qu'il n'en faut au code pour passer a la ligne suivante 
+            return data = await rawData.json() //on convertit les données en un objet json et encore une fois on utilise await car la conversion prend plus de temps
+        }
+    
+        console.log(await fetcher()) //afin de ne pas afficher les données pendant qu'elles sont manipulées, on ajoute un await (tu peux enlever l'await si tu veux voir ce qu'il se passe)
+        
+        document.getElementById()
+})();
