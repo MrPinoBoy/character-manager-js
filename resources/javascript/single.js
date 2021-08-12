18d20473924c581d5414df967b070c3194927af6
Engie-Paquot <angelique.paquot.pro@gmail.com>
2021-08-12 15:38:17 +0200

    script js page single

--- resources/javascript/single.js
+++ resources/javascript/single.js
@@ -1,14 +1,22 @@
 (async() =>  {
         // const template = document.getElementById("tpl-character")
         // const target = document.getElementById("target")
+        const img = document.getElementById("character-img")
+        const name = document.getElementById("character-name")
+        const smallDescription = document.getElementById("character-small-description")
+        const longueDescription = document.getElementById("character-long-description")
         const currentId = window.location.hash.split('#')// on va chercher une information dans l'url, à partir du #, et split sert à se débarasser du #
         const fetcher = async() => { //on crée une fonction async (pour pouvoir utiliser await)
             let rawData = await fetch(`https://character-database.becode.xyz/characters/${currentId[1]}`)// on va chercher les informations qui correspondent au bon id. On précise [1] parce que split crée une array de deux éléments, un avec ce qu'il y a avant le # et un avec ce qu'il y a après 
-            return data = await rawData.json() //on convertit les données en un objet json et encore une fois on utilise await car la conversion prend plus de temps
+            return character = await rawData.json() //on convertit les données en un objet json et encore une fois on utilise await car la conversion prend plus de temps
         }
     
         //console.log(await fetcher()) //afin de ne pas afficher les données pendant qu'elles sont manipulées, on ajoute un await (tu peux enlever l'await si tu veux voir ce qu'il se passe)
-        await fetcher()
-        
+        await fetcher()        
+        img.setAttribute("src", `data:image/jpeg;base64,${character.image}`) // pour les img il faut changer la src d'où setAttribute, `data:image/jpeg;base64,${data.image}` convertir un url de données en images
+        name.innerHTML = character.name
+        smallDescription.innerHTML = character.shortDescription
+        longueDescription.innerHTML = character.description
+        console.log(character);
         
 })();
