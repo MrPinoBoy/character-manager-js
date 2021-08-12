5c1654398547c11d93101022bb884ed49762c3f3
Thomas Beaupain <beaupain.thomas@gmail.com>
2021-08-12 06:38:38 -0700

    put all input values in object and check if input windows are empty

--- resources/javascript/create.js
+++ resources/javascript/create.js
@@ -1,7 +1,16 @@
 (async() =>  {
-    const image = document.getElementById("character-image-upload")
+    const allInputs = document.querySelectorAll("input")
+    const newImage = document.getElementById("character-image-upload")
     const preview = document.getElementById("character-image-preview")
-    function readImage(file) {
+    const newName = document.getElementById("input-character-name")
+    const newSmallDescription = document.getElementById("input-character-small-description")
+    const newLongDescription = document.getElementById("input-character-long-description")
+    const saveNewCharacter = document.getElementById("button-save")
+    const cancelNewCharacter = document.getElementById("button-cancel")
+    const newCharacter = new Object
+
+    allInputs.forEach(element => element.value = "")//on vide tous les inputs
+    async function readImage(file) {
     // Check if the file is an image.
     if (file.type && !file.type.startsWith('image/')) {
       console.log('File is not an image.', file.type, file);
@@ -10,14 +19,31 @@
   
     const reader = new FileReader();//lis le contenu d'un fichier
     reader.addEventListener('load', (event) => {//l'event se lance après la ligne 16
-        const imageDataUrl = event.target.result//result donne le résultat de la ligne 16
-        preview.setAttribute("src", imageDataUrl);//on remplace la source par l'url
+        const imageDataUrl = reader.result//result donne le résultat de la ligne 16
+        preview.src = imageDataUrl;//on remplace la source par l'url
+        const imageUrlSeparator = imageDataUrl.split(",")
+        newCharacter.image = imageUrlSeparator[1]
     });
-    reader.readAsDataURL(file);//transforme le fichier en data url
+    ;//transforme le fichier en data url
+    reader.readAsDataURL(file)
   }
-    image.addEventListener("change", function(event){
+
+    newImage.addEventListener("change", async function(event){
         let file = event.target.files//on va chercher le fichier qu'on a input qui est enregistré comme une array
-        console.log(file)
-        readImage(file[0])//on utilise le fichier dans la fonction
+        await readImage(file[0])//on utilise le fichier dans la fonction
+    })
+
+    saveNewCharacter.addEventListener("click", async()=> {
+        if (newName.value != "" && newSmallDescription.value != "" && newLongDescription.value != "" && newImage.value != ""){
+            newCharacter.name = newName.value
+            newCharacter.shortDescription = newSmallDescription.value
+            newCharacter.description = newLongDescription.value
+
+        } else {
+            alert("erreur")
+        }
+    })
+
+    cancelNewCharacter.addEventListener("click", function(){
     })
 })();
